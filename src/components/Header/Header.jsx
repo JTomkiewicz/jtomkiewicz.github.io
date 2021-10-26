a7e807d2af0e8855e074a288ec126483b8a10cd1
Jakub Tomkiewicz <kubatomki@gmail.com>
2021-10-26 22:05:12 +0200

    sticky navbar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import './Header.css';
 
 const Header = () => {
+  const [scroll, setScroll] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      setScroll(window.scrollY > 40);
+    });
+  }, []);
+
   return (
-    <header className="mb-auto">
-      <div>
+    <header className={scroll ? 'bg-dark sticky-top header-scroll' : null}>
+      <div className="mb-auto">
         <h3 className="float-md-start mb-0">Jakub Tomkiewicz</h3>
         <nav className="nav nav-masthead justify-content-center float-md-end">
           <a className="nav-link active" href="#id">
