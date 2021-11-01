fba1e418de9a70f4607e48d143ce97b048fb95a1
Jakub Tomkiewicz <kubatomki@gmail.com>
2021-11-01 21:38:04 +0100

    table with technology stack

--- src/components/MainPage/AboutMe/AboutMe.jsx
+++ src/components/MainPage/AboutMe/AboutMe.jsx
@@ -10,6 +10,40 @@ const AboutMe = () => {
         Ambitious 4th year Computer Science student that loves creating websites
         and wants to become a master at single-page applications.
       </p>
+      <p>
+        <table style={{ margin: 'auto' }}>
+          <tr>
+            <td style={{ textAlign: 'left' }}>HTML, CSS</td>
+            <td style={{ textAlign: 'right' }}>
+              &#11088;&nbsp;&#11088;&nbsp;&#11088;
+            </td>
+          </tr>
+          <tr>
+            <td style={{ textAlign: 'left' }}>JS</td>
+            <td style={{ textAlign: 'right' }}>
+              &#11088;&nbsp;&#11088;&nbsp;&#11088;&nbsp;
+            </td>
+          </tr>
+          <tr>
+            <td style={{ textAlign: 'left' }}>React</td>
+            <td style={{ textAlign: 'right' }}>&#11088;&nbsp;&#11088;</td>
+          </tr>
+          <tr>
+            <td style={{ textAlign: 'left' }}>jQuery</td>
+            <td style={{ textAlign: 'right' }}>&#11088;&nbsp;&#11088;</td>
+          </tr>
+          <tr>
+            <td style={{ textAlign: 'left' }}>SQL</td>
+            <td style={{ textAlign: 'right' }}>
+              &#11088;&nbsp;&#11088;&nbsp;&#11088;&nbsp;&#11088;
+            </td>
+          </tr>
+          <tr>
+            <td style={{ textAlign: 'left' }}>GIT</td>
+            <td>&#11088;&nbsp;&#11088;&nbsp;&#11088;&nbsp;&#11088;</td>
+          </tr>
+        </table>
+      </p>
     </section>
   );
 };
