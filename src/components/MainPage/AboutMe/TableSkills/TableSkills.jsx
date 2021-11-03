0aa2affe158deaa4b864a2c5f4bc03adb2ad179c
Jakub Tomkiewicz <kubatomki@gmail.com>
2021-11-03 18:02:32 +0100

    table as component & hrefs

---
+++ src/components/MainPage/AboutMe/TableSkills/TableSkills.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+const TableSkills = () => {
+  return (
+    <table style={{ margin: 'auto' }}>
+      <tbody>
+        <tr>
+          <td style={{ textAlign: 'left' }}>HTML, CSS</td>
+          <td style={{ textAlign: 'right' }}>
+            &#11088;&nbsp;&#11088;&nbsp;&#11088;
+          </td>
+        </tr>
+        <tr>
+          <td style={{ textAlign: 'left' }}>JS</td>
+          <td style={{ textAlign: 'right' }}>
+            &#11088;&nbsp;&#11088;&nbsp;&#11088;
+          </td>
+        </tr>
+        <tr>
+          <td style={{ textAlign: 'left' }}>React</td>
+          <td style={{ textAlign: 'right' }}>&#11088;&nbsp;&#11088;</td>
+        </tr>
+        <tr>
+          <td style={{ textAlign: 'left' }}>jQuery</td>
+          <td style={{ textAlign: 'right' }}>&#11088;&nbsp;&#11088;</td>
+        </tr>
+        <tr>
+          <td style={{ textAlign: 'left' }}>SQL</td>
+          <td style={{ textAlign: 'right' }}>
+            &#11088;&nbsp;&#11088;&nbsp;&#11088;&nbsp;&#11088;
+          </td>
+        </tr>
+        <tr>
+          <td style={{ textAlign: 'left' }}>GIT</td>
+          <td>&#11088;&nbsp;&#11088;&nbsp;&#11088;&nbsp;&#11088;</td>
+        </tr>
+      </tbody>
+    </table>
+  );
+};
+
+export default TableSkills;
